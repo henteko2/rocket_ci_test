28084b4318c883b0e6b9f72f056a73f18c5856dd
henteko <henteko07@gmail.com>
2016-09-29 19:08:36 +0900

    add rocket.sh

---
+++ rocket.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -u -o -x -v
+
+echo $ROCKET_WORKSPACE
+echo $ROCKET_ARTIFACTS
+echo $ANDROID_HOME
+echo $ANDROID_SDK_HOME
+echo $ANDROID_SDK_ROOT
+echo $ANDROID_NDK_HOME
+echo $ANDROID_NDK_ROOT
+
+./gradlew assemble

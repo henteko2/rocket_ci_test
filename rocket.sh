167e676167a5acc630ea222bcecc95e98597e1ab
henteko <henteko07@gmail.com>
2016-09-29 20:26:25 +0900

    add license

--- rocket.sh
+++ rocket.sh
@@ -12,4 +12,8 @@ echo $ANDROID_SDK_ROOT
 echo $ANDROID_NDK_HOME
 echo $ANDROID_NDK_ROOT
 
+echo $ANDROID_HOME/licenses/android-sdk-license
+echo '8933bad161af4178b1185d1a37fbf41ea5269c55' > $ANDROID_HOME/licenses/android-sdk-license
+echo $ANDROID_HOME/licenses/android-sdk-license
+
 ./gradlew assemble

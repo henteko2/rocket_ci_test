656b8e1d0e6327edae558070579baf4642c27d95
henteko <henteko07@gmail.com>
2016-10-14 18:18:53 +0900

    test

--- rocket.sh
+++ rocket.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-ls -all
-
 cd $ROCKET_WORKSPACE
 
 set -u -o -x -v
@@ -13,10 +11,5 @@ echo $ANDROID_SDK_HOME
 echo $ANDROID_SDK_ROOT
 echo $ANDROID_NDK_HOME
 echo $ANDROID_NDK_ROOT
-
-cat $ANDROID_HOME/licenses/android-sdk-license
-mkdir -p $ANDROID_HOME/licenses/
-echo '8933bad161af4178b1185d1a37fbf41ea5269c55' > $ANDROID_HOME/licenses/android-sdk-license
-cat $ANDROID_HOME/licenses/android-sdk-license
 
 ./gradlew assemble
